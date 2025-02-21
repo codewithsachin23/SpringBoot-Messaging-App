78e41b49eef71392a5c148ee78712f0e933b80cc
sachin shukla <sachinshukla232003@gmail.com>
2025-02-21 15:13:59 +0530

    [Sachin Shukla]Add.uc4 using Post to model user

--- src/main/java/com/controllers/MessagingApplication.java
+++ src/main/java/com/controllers/MessagingApplication.java
@@ -1,8 +1,16 @@
 package com.controllers;
 
+import com.model.UserDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessagingApplication {
+    @PostMapping("/post")
+    public String hello4(@RequestBody UserDTO user) {
+        return "Hello "+ user.getFirstName() + " " + user.getLastName() +" from BridgeLab";
+
+    }
 
 }
