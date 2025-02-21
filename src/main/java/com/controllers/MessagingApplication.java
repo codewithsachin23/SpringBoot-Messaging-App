8c223264bfc3b0f323f6f63aaa15e9fcb42d15ca
sachin shukla <sachinshukla232003@gmail.com>
2025-02-21 15:15:42 +0530

    [Sachin Shukla]Add.uc5 using Put to show message

--- src/main/java/com/controllers/MessagingApplication.java
+++ src/main/java/com/controllers/MessagingApplication.java
@@ -1,8 +1,15 @@
 package com.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessagingApplication {
+    @PutMapping("Hello/{firstName}")
+    public String hello5(@PathVariable String firstName, @RequestParam String lastName) {
+        return "Hello "+ firstName + " " + lastName +" from BridgeLab";
+    }
 
 }
