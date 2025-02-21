88e2b7f55bc4816a81ab22868d93ad1c5349c0e9
sachin shukla <sachinshukla232003@gmail.com>
2025-02-21 15:43:26 +0530

    [Sachin Shukla]Add.uc1 message using get

--- src/main/java/com/controllers/MessagingApplication.java
+++ src/main/java/com/controllers/MessagingApplication.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessagingApplication {
+
     @GetMapping("/hello")
     public String hello() {
+
         return "Hello from BridgeLab";
 
     }
