15abb86051a2415f4ca4df5c8f9ace5af1881f92
sachin shukla <sachinshukla232003@gmail.com>
2025-02-21 15:09:01 +0530

    [Sachin Shukla]Add.uc2 hello message to user with path variable

--- src/main/java/com/controllers/MessagingApplication.java
+++ src/main/java/com/controllers/MessagingApplication.java
@@ -1,8 +1,14 @@
 package com.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessagingApplication {
+    @GetMapping("Hello/{name}")
+    public String hello3(@PathVariable String name) {
+        return "Hello "+ name +" from BridgeLab";
+    }
 
 }
