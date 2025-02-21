d03b7a1af1ae1de82c08cfcd6d3b9e18fb0c0356
sachin shukla <sachinshukla232003@gmail.com>
2025-02-21 15:07:09 +0530

    [Sachin Shukla]Add.uc2 hello message to user with Query

--- src/main/java/com/controllers/MessagingApplication.java
+++ src/main/java/com/controllers/MessagingApplication.java
@@ -1,8 +1,16 @@
 package com.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessagingApplication {
+
+
+    @GetMapping("/q")
+    public String hello2(@RequestParam String name) {
+        return "Hello "+ name +" from BridgeLab";
+    }
 
 }
