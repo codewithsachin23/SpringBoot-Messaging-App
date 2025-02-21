f0b7b39fceda494174665b35e4dab1c4cb5623fb
sachin shukla <sachinshukla232003@gmail.com>
2025-02-21 15:03:47 +0530

    [Sachin Shukla]Add.uc1 hello message to user using GET

--- src/main/java/com/controllers/MessagingApplication.java
+++ src/main/java/com/controllers/MessagingApplication.java
@@ -1,8 +1,13 @@
 package com.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessagingApplication {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from BridgeLabz";
+    }
 
 }
